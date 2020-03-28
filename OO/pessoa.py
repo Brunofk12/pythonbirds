3c2a090db15e7ec2eadb9e809adb9e5119dc7809
Bruno Ferro <bruno.kopelingh@hotmail.com>
2020-03-28 14:20:51 -0300

    Criados atributos de instancia nome e idade

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,8 +1,15 @@
 class Pessoa:
+    def __init__(self, nome = , idade = 35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Oi {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Tatiana')
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'bruno'
+    print(p.idade)
